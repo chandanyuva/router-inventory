c94f7880a5155b28c65794058f62e262128a3375
Chandan K P <chandanyuva1511@gmail.com>
2025-09-21 22:04:49 +0530

    Update pool.js

--- public/db/pool.js
+++ public/db/pool.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
 
 // Again, this should be read from an environment variable
+// const pool = new Pool({
+//   connectionString: "postgresql://chandan:yuva@localhost:5432/iop_devices"
+// });
 const pool = new Pool({
-  connectionString: "postgresql://chandan:yuva@localhost:5432/iop_devices"
+  connectionString: process.env.DATABASE_URL, // single env var
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
